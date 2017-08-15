10311b06557f4f8bf201ee706af1769a72909500
Alexis Tacnet <alexistacnet@gmail.com>
2017-08-15 23:39:03 +0200

    Fix the OpenVPN status

--- core/vpn_status.py
+++ core/vpn_status.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 
-import os
-vpns = [vpn for vpn in os.listdir('/run/openvpn-client')  if vpn[-3:] == 'pid']
+import subprocess
+import re
 
-if len(vpns) > 0:
-    print('   Connected to {}'.format(vpns[0][:-4]))
+p = subprocess.Popen(["ps", "aux"], stdout=subprocess.PIPE)
+out, err = p.communicate()
+
+string_output = str(out)
+processes = string_output.split('\\n')
+openvpns = list(filter(lambda x: '/openvpn' in x, processes))
+
+if len(openvpns) > 0:
+    p = re.compile("(\S*)\.conf")
+    match = p.search(openvpns[0])
+    if match:
+        vpn = match.group(1)
+        print('   Connected to {}'.format(vpn))
+    else:
+        print('   Connected')
 else:
     print('   Not connected')
