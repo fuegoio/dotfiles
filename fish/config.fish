ffa240e77c842f28905abb72027f863d45ebe936
Alexis Tacnet <alexistacnet@gmail.com>
2018-05-02 09:14:28 +0200

    Fix colors

--- fish/config.fish
+++ fish/config.fish
@@ -32,33 +32,27 @@ function fish_prompt
   set -l last_status $status
   set -l red (set_color -o red)
   set -l blue (set_color -o blue)
-  set -l lightb (set_color -o magenta)
-  set -l yellow (set_color -o yellow)
+  set -l purple (set_color -o yellow)
   set -l normal (set_color normal)
 
-  if test $last_status = 0
-      set status_indicator "$blue✔ "
-  else
-      set status_indicator "$red✗ "
-  end
-  set -l host $lightb(hostname)
+  set -l host $purple(hostname)
   set -l cwd $blue(basename (prompt_pwd))
+  set -l arrow ""
 
   if [ (_git_branch_name) ]
-
     if test (_git_branch_name) = 'master'
       set -l git_branch (_git_branch_name)
-      set git_info "$normal ($red$git_branch$normal)"
+      set git_info "$normal ($purple $git_branch$normal)"
     else if test (_git_branch_name) = 'dev'
       set -l git_branch (_git_branch_name)
-      set git_info "$normal ($yellow$git_branch$normal)"
+      set git_info "$normal ($red $git_branch$normal)"
     else
       set -l git_branch (_git_branch_name)
-      set git_info "$normal ($blue$git_branch$normal)"
+      set git_info "$normal ($blue $git_branch$normal)"
     end
 
     if [ (_is_git_dirty) ]
-      set -l dirty "$red ✗"
+      set -l dirty "$red "
       set git_info "$git_info$dirty"
     end
   end
@@ -68,5 +62,5 @@ function fish_prompt
 	echo The last command took (math "$CMD_DURATION/1000") seconds.
   end
 
-  echo -n -s $status_indicator $host ' ' $cwd $git_info $normal ' '
+  echo -n -s $host ' ' $cwd $git_info $normal ' ' $arrow ' '
 end
