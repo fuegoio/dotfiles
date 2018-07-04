39f99dcd19fe7cf7493faf1d5c0a7316c73e8cb9
Alexis Tacnet <alexistacnet@gmail.com>
2018-07-04 22:16:30 +0800

    Add right prompt to fish

--- fish/config.fish
+++ fish/config.fish
@@ -14,9 +14,16 @@ alias vpnoff="sudo wg-quick down wg0"
 set -x EDITOR vim
 set -gx ANDROID_HOME $HOME/Android/Sdk
 set -gx npm_config_prefix $HOME/.node_modules
-
 set -U fish_user_paths $ANDROID_HOME/tools $ANDROID_HOME/platform-tools $HOME/.node_modules/bin
-set -U fish_greeting "  Welcome to Argo. Go faire du sale."
+
+set red (set_color -o red)
+set blue (set_color -o blue)
+set purple (set_color -o magenta)
+set green (set_color -o cyan)
+set yellow (set_color -o yellow)
+set normal (set_color normal)
+
+set -U fish_greeting "$blue $normal Welcome to Argo. Go faire du sale."
 
 fish_vi_key_bindings insert
 function fish_user_key_bindings
@@ -37,33 +44,37 @@ function _is_git_dirty
   echo (command git status -s --ignore-submodules=dirty ^/dev/null)
 end
 
+function _is_git_not_pushed
+  echo (command git log origin/master..HEAD ^/dev/null)
+end
+
 set fish_color_command blue
 
 function fish_prompt
   set -l last_status $status
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l purple (set_color -o magenta)
-  set -l yellow (set_color -o yellow)
-  set -l normal (set_color normal)
-
-  set -l host $purple(hostname)
-  set -l cwd $blue(basename (prompt_pwd))
+  set -l host $blue(hostname)
   set -l arrow ""
-
-  if [ (_git_branch_name) ]
-      set -l git_branch (_git_branch_name)
-      if [ (_is_git_dirty) ]
-          set git_info "$normal ($yellow $git_branch$normal)"
-      else
-          set git_info "$normal ($red $git_branch$normal)"
-      end
-  end
 
   # Notify if a command took more than 10 sec
   if [ "$CMD_DURATION" -gt 10000 ]
 	echo The last command took (math "$CMD_DURATION/1000") seconds.
   end
 
-  echo -n -s $host ' ' $cwd $git_info $normal ' ' $arrow ' '
+  echo -n -s $host ' ' $normal $arrow ' '
+end
+
+function fish_right_prompt
+  set -l cwd $purple (prompt_pwd)
+
+  if [ (_git_branch_name) ]
+    set git_branch "$blue  " (_git_branch_name)
+    if [ (_is_git_not_pushed) ]
+      set git_not_pushed "$green  "
+    end
+    if [ (_is_git_dirty) ]
+      set git_dirty "$red  "
+    end
+  end
+
+  echo -n -s $cwd $git_not_pushed $git_dirty $git_branch $normal
 end
