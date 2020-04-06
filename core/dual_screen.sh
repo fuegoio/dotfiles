a43d21dfc8b0cfdd11ca5b53e49a8d003fbd97d4
Alexis Tacnet <alexistacnet@gmail.com>
2020-04-06 02:09:29 +0200

    Add polybar utils

--- core/dual_screen.sh
+++ core/dual_screen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP2 --off
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 2560x973 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
